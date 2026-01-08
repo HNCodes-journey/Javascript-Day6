818f89c7ec7092e7694c306637fe40a94b8ddcfc
HNCodes-journey <codewithussnain1@gmail.com>
2026-01-09 00:01:54 +0500

    Updated Code files of Day6

--- script.js
+++ script.js
@@ -51,3 +51,65 @@ let access = LoggedIn && hastoken ? "Allow" : "Deny";
 console.log(access);
 
 //Output : Deny
+
+//Question 7:
+let b =7;
+++b;
+console.log(b);
+ //output : 8
+
+ //Question 8:
+ let d = 3;
+ let e = d++ ;
+ console.log(d,e);
+// output : 4 , 3
+
+//Question 9:
+let p =4;
+let q = ++p;
+console.log(p,q);
+//Output : 5 5
+
+//Question 10:
+let m = 10;
+console.log(m--); //line 1
+console.log(m);    //Line 2
+//Output : 10 , 9
+
+//Question 11:
+let n = 5; 
+let result = n++ + ++n;
+console.log(result);
+
+// Output : 12
+
+//Question 12:
+let likes = 100;
+function likepost(){
+    return ++likes;
+}
+console.log(likepost());
+console.log(likes);
+
+//Output : 101
+//         101
+
+//Question 13:
+let count = 5;
+ if (count-- ===5 ){
+    console.log("Matched"); 
+ }
+ else{
+    console.log("Not Matched");
+ }
+//Output : Matched
+
+
+
+
+
+
+
+
+
+ 
